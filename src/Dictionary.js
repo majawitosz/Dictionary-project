da5ee5859183a2e1cb7bf53145b373f055f22990
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-09-28 22:29:31 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,6 @@
-import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
@@ -8,12 +8,11 @@ export default function Dictionary() {
   function handleResposne(resposne) {
     console.log(resposne.data[0]);
   }
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-  axios.get(apiUrl).then(handleResposne);
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResposne);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
