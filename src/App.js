7c43922dc5d954ab87f730ae1d0e004f1519d991
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-09-28 14:27:01 +0200

    Adding search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">What word you are looking for?</header>
+        <Dictionary />
+        <footer className="footer">Coded by Maja Witosz</footer>
+      </div>
     </div>
   );
 }
