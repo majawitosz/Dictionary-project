f2b68d3975f0e4fb6eda6e2eb5dfd5250e500ea0
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-10-06 22:48:50 +0200

    Added volume icon

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,8 +1,12 @@
+import "./Phonetics.css";
+import { FaVolumeUp } from "react-icons/fa";
 export default function Phonetics(props) {
   return (
     <div className="Phonetics">
       <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-        <button>🔊</button>
+        <button>
+          <FaVolumeUp className="volumeIcon" />
+        </button>
       </a>
       <br />
       {props.phonetics.text}
